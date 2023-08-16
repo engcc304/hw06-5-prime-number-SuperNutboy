1d6429cdfad45c744989e28f602c4874d9e25d47
SuperNutboy <natthapong_ta66@live.rmutl.ac.th>
2023-08-17 02:01:47 +0700

    Uppspsgsgsgshjd;fgljsdkwgsw

--- Homework/HW06/hw06-5-prime-number.cpp
+++ Homework/HW06/hw06-5-prime-number.cpp
@@ -14,3 +14,32 @@
     Output:
         97 89 83 79 73 71 67 61 59 53 47 43 41 37 31 29 23 19 17 13 11 7 5 3 2
 */
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int n; 
+    printf("Enter number: ");
+    scanf("%d", &n); 
+
+    printf("Output:\n");
+    for (int i = n; i > 1; i--) {
+        if (isPrime(i)) {
+            printf("%d ", i); 
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
